d5f580d893c777801dffbb54dd6ba92b5f9d18e8
Jason Sydes <sydes@uoregon.edu>
2015-05-04 14:01:15 -0700

    Hopefully fixed up versioning.
    Previously: VERSION was the master file, and prost/_version.py was 
    auto generated by the build.sh file.
    
    Now: prost/_version.py is the master file, and VERSION just symlinks to
    it.
    
    Hopefully this will play well with git-flow (AVH), git-flow-hooks, 
    setup tools, etc etc.  We’ll see.

--- setup.py
+++ setup.py
@@ -8,9 +8,20 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+# Version (see http://stackoverflow.com/a/7071358)
+import re
+VERSIONFILE="prost/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
 setup(
     name='prost',
-    version = file("VERSION").readline().rstrip(),
+    version = verstr,
 
     description='An application to quantify and annotate miRNA expression.',
     long_description=long_description,
