4e2195e2fed8eb3ec91256d8b227159439d76e3f
Jason Sydes <sydes@uoregon.edu>
2015-05-04 15:13:04 -0700

    New approach: git flow manages both version files.
    Ok, the previous approach didn’t work.
    
    This approach does the following:
    * Restore VERSION as the authoritative source of version info.
    * modified internal git flow hooks to also update prost/_version.py

--- setup.py
+++ setup.py
@@ -8,20 +8,9 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-# Version (see http://stackoverflow.com/a/7071358)
-import re
-VERSIONFILE="prost/_version.py"
-verstrline = open(VERSIONFILE, "rt").read()
-VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-mo = re.search(VSRE, verstrline, re.M)
-if mo:
-    verstr = mo.group(1)
-else:
-    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
-
 setup(
     name='prost',
-    version = verstr,
+    version = file("VERSION").readline().rstrip(),
 
     description='An application to quantify and annotate miRNA expression.',
     long_description=long_description,
